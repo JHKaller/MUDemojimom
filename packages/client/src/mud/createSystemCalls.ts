5919d0d482325f8d63e7a9e111d8e6fa0df3b853
JHKaller <john.kaller@hotmail.de>
2023-07-18 12:54:47 +0200

    tutorial

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -3,38 +3,86 @@ import { uuid, awaitStreamValue } from "@latticexyz/utils";
 import { MonsterCatchResult } from "../monsterCatchResult";
 import { ClientComponents } from "./createClientComponents";
 import { SetupNetworkResult } from "./setupNetwork";
-
+ 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
-
+ 
 export function createSystemCalls(
-  { singletonEntity, playerEntity, worldSend, txReduced$ }: SetupNetworkResult,
-  components: ClientComponents
+  { playerEntity, worldSend, txReduced$ }: SetupNetworkResult,
+  { Player, Position }: ClientComponents
 ) {
   const moveTo = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+ 
+    const positionId = uuid();
+    Position.addOverride(positionId, {
+      entity: playerEntity,
+      value: { x, y },
+    });
+ 
+    try {
+      const tx = await worldSend("move", [x, y]);
+      await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+    } finally {
+      Position.removeOverride(positionId);
+    }
   };
-
+ 
   const moveBy = async (deltaX: number, deltaY: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+ 
+    const playerPosition = getComponentValue(Position, playerEntity);
+    if (!playerPosition) {
+      console.warn("cannot moveBy without a player position, not yet spawned?");
+      return;
+    }
+ 
+    await moveTo(playerPosition.x + deltaX, playerPosition.y + deltaY);
   };
-
+ 
   const spawn = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+ 
+    const canSpawn = getComponentValue(Player, playerEntity)?.value !== true;
+    if (!canSpawn) {
+      throw new Error("already spawned");
+    }
+ 
+    const positionId = uuid();
+    Position.addOverride(positionId, {
+      entity: playerEntity,
+      value: { x, y },
+    });
+    const playerId = uuid();
+    Player.addOverride(playerId, {
+      entity: playerEntity,
+      value: { value: true },
+    });
+ 
+    try {
+      const tx = await worldSend("spawn", [x, y]);
+      await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+    } finally {
+      Position.removeOverride(positionId);
+      Player.removeOverride(playerId);
+    }
   };
-
+ 
   const throwBall = async () => {
     // TODO
     return null as any;
   };
-
+ 
   const fleeEncounter = async () => {
     // TODO
     return null as any;
   };
-
+ 
   return {
     moveTo,
     moveBy,
